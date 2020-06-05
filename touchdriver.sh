9757f2dd24867e469600f54c4fa0c438ef3a85a2
bigbiff <bigbiff@teamw.in>
2020-06-05 19:10:20 -0400

    add missing files
    Change-Id: I408ebecd6e6b7d401826858e85098af149061948

--- touchdriver.sh
+++ touchdriver.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 
 fastboot=$(getprop ro.boot.bootreason | cut -d, -F2)
-if [[ $fastboot == bootloader || $fastboot == "longkey" ]]
+if [[ $fastboot == bootloader || $fastboot == "longkey" || $fastboot == "reboot" ]]
 then
 	insmod /sbin/videobuf2-vmalloc.ko
 	insmod /sbin/videobuf2-memops.ko 
