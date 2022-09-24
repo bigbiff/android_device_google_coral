3829e08d3d6b37d889587544e52099e1b386b02e
bigbiff <bigbiff@teamw.in>
2022-09-24 14:28:42 -0400

    coral: add coral product for twrp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -24,6 +24,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_coral.mk \
     $(LOCAL_DIR)/aosp_flame.mk \
     $(LOCAL_DIR)/twrp_flame.mk \
+    $(LOCAL_DIR)/twrp_coral.mk \
     $(LOCAL_DIR)/aosp_coral_hwasan.mk \
     $(LOCAL_DIR)/aosp_flame_hwasan.mk \
 
